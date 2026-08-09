58d9eed39800670ca4c1f08a774a9bc3adf15c97
Rafael Ramos <ramosrafh@gmail.com>
2026-08-09 18:57:43 -0300

    add: caddy

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -5,6 +5,7 @@
     ../../modules/nixos/base
     ../../modules/nixos/profiles/server.nix
     ../../modules/nixos/security/secure-boot.nix
+    ../../modules/nixos/services/caddy.nix
     ../../modules/nixos/services/k3s.nix
     ../../modules/nixos/services/netbird.nix
     ../../modules/nixos/services/ssh.nix

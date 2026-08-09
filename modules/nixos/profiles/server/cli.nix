ddfa2615dad8c890c247e772e37fb022f6e2f842
Rafael Ramos <ramosrafh@gmail.com>
2026-08-09 17:47:32 -0300

    fix: autuin

--- modules/nixos/profiles/server/cli.nix
+++ modules/nixos/profiles/server/cli.nix
@@ -18,6 +18,26 @@
     etc."atuin/config.toml".text = ''
       auto_sync = false
       update_check = false
+      style = "compact"
+      inline_height = 14
+      filter_mode_shell_up_key_binding = "directory"
+      search_mode = "fuzzy"
+      invert = true
+      enter_accept = true
+      keymap_mode = "vim-insert"
+      show_preview = true
+      show_help = false
+      exit_mode = "return-original"
+
+      history_filter = [
+        "^ls",
+        "^cd",
+        "^exit",
+        "^clear",
+      ]
+
+      [sync]
+      records = false
     '';
     sessionVariables.ATUIN_CONFIG_DIR = "/etc/atuin";
   };
@@ -25,7 +45,7 @@
   programs.fish.interactiveShellInit = ''
     ${pkgs.zoxide}/bin/zoxide init fish | source
     ${pkgs.fzf}/bin/fzf --fish | source
-    ${pkgs.atuin}/bin/atuin init fish | source
+    ${pkgs.atuin}/bin/atuin init fish --disable-up-arrow | source
 
     function nrs --description "Switch the server configuration"
       sudo nixos-rebuild switch --flake "path:$HOME/nixconfig#server"

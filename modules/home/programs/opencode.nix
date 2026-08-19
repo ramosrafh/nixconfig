84ea7f4ec881bad8de689ea567f0d995ecd94c48
Rafael Ramos <ramosrafh@gmail.com>
2026-08-19 19:14:30 -0300

    add: opencode glm and kimi

--- modules/home/programs/opencode.nix
+++ modules/home/programs/opencode.nix
@@ -6,5 +6,16 @@
   xdg.configFile."opencode/opencode.json".text = builtins.toJSON {
     "$schema" = "https://opencode.ai/config.json";
     provider.openrouter.options.apiKey = "{env:OPENROUTER_API_KEY}";
+    provider.driva = {
+      npm = "@ai-sdk/openai-compatible";
+      options = {
+        baseURL = "http://vpn-driva.netbird.driva.io:8317/v1";
+        apiKey = "{env:OPENAI_API_KEY}";
+      };
+      models = {
+        "glm/glm-5.3".name = "driva/glm-5.3";
+        "kimi/kimi-k3".name = "driva/kimi-k3";
+      };
+    };
   };
 }
